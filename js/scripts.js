4b779e66e102e519477e4b859c53e6075957cedc
Reid Ashwill <reidashwill@gmail.com>
2020-03-24 10:15:15 -0700

    revert page back to starting point for next excercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
+
 });
 
